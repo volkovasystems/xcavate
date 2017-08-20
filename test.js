51affd3082102c0ddfd5e00a18156d997d18dfe1
Vinse Vinalon <vinsevinalon@gmail.com>
2017-08-20 09:26:03 +0800

    Update client/server unit test.

--- test.js
+++ test.js
@@ -68,7 +68,7 @@ const xcavate = require( "./xcavate.js" );
 //: @server:
 
 describe( "xcavate", ( ) => {
-	
+
 	describe( `"xcavate( "hello", data )"`, ( ) => {
 
 		describe( `"xcavate( Symbol( "hello" ), data "`, ( ) => {
@@ -98,21 +98,3 @@ describe( "xcavate", ( ) => {
 
 
 
-
-
-
-
-
-
-// const assert = require( "assert" );
-// const xcavate = require( "./xcavate.js" );
-
-// let symbol = Symbol( "hello" );
-// let data = { };
-// data[ symbol ] = symbol;
-
-// assert.equal( xcavate( "hello", data ), symbol, "should be equal to Symbol( 'hello' )" );
-
-// assert.equal( xcavate( Symbol( "hello" ), data ), symbol, "should be equal to Symbol( 'hello' )" );
-
-// console.log( "ok" );
